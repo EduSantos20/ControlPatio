0724fedacc15385014f0a07618f1fa5987489fd3
EduSantos20 <eduardosantosgt18@gmail.com>
2025-07-12 01:48:36 -0300

    feat:config as rotas

--- src/main/java/com/example/controlPatio/IndexControlPatio.java
+++ src/main/java/com/example/controlPatio/IndexControlPatio.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class IndexControlPatio {
   @RequestMapping("/")
   public String index() {
-    return "login";
+    return "index";
   }
 }
